49b3cdcffc3849ae4df2159471b030a0495cd33e
Aleksey Novikov <asernovikov@sberbank.ru>
2021-05-10 18:49:21 +0300

    issue close #9

--- src/routes/person/files/index.tsx
+++ src/routes/person/files/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState, ChangeEvent, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import useClassnames, { IStyle } from 'hook/use-classnames';
@@ -9,6 +9,12 @@ import IconFileDocument from 'component/icons/file-document';
 import IconFileImage from 'component/icons/file-image';
 import style from './index.module.pcss';
 
+export interface IFile {
+    name: string,
+    type?: string,
+    url?: string
+}
+
 export interface IProps {
     className?: string | IStyle,
     id?: string
@@ -17,6 +23,69 @@ export interface IProps {
 export const Files = (props: IProps) => {
     const cn = useClassnames(style, props.className, true);
     const { t } = useTranslation();
+    const [list, setList] = useState<Array<IFile>>([{
+        name: 'Резюме',
+        type: 'application/pdf',
+        url : 'https://filebin.net/a0900c3i0uy8jpkg/168725.png'
+    }, {
+        name: 'Полный список компетенций',
+        url : 'https://filebin.net/a0900c3i0uy8jpkg/168725.png'
+    }, {
+        name: 'Отзыв от работодателя',
+        url : 'https://filebin.net/a0900c3i0uy8jpkg/168725.png'
+    }, {
+        name: 'Сертификат о повышении квалификации',
+        type: 'image/png',
+        url : 'https://filebin.net/a0900c3i0uy8jpkg/168725.png'
+    }]);
+
+    const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        const files: Array<IFile> = [];
+
+        if(e.target.files?.length) {
+            // eslint-disable-next-line @typescript-eslint/prefer-for-of
+            for(let i = 0; i < e.target.files.length; i++) {
+                files.push({
+                    name: e.target.files[i].name,
+                    type: e.target.files[i].type
+                });
+            }
+        }
+
+        setList((state) => [
+            ...state,
+            ...files
+        ]);
+    }, []);
+
+    const elFileIcon = useCallback((type?: string) => {
+        switch (type) {
+            case 'image/png':
+            case 'image/jpg':
+                return <IconFileImage />;
+
+            case 'application/pdf':
+                return <IconFilePdf />;
+
+            default:
+                return <IconFileDocument />;
+        }
+    }, []);
+
+    const elList = useMemo(() => {
+        if(list.length) {
+            return (
+                <ul className={cn('files__list')}>
+                    {list.map((item, index) => (
+                        <li key={index} className={cn('files__list-item')}>
+                            {elFileIcon(item.type)}
+                            <a href={item.url} target="_blank" rel="noreferrer">{item.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }, [list]);
 
     return (
         <div id={props.id} className={cn('files')}>
@@ -24,30 +93,18 @@ export const Files = (props: IProps) => {
             <form className={cn('files__controls')}>
                 <label className={cn('files__control')}>
                     <IconPlus />
-                    <input type="file" name="file" className={cn('files__input-file')} />
+                    <input
+                        type="file"
+                        name="file"
+                        className={cn('files__input-file')}
+                        onChange={onChange}
+                    />
                 </label>
                 <div className={cn('files__control', 'files__control_disable')}>
                     <IconPencil />
                 </div>
             </form>
-            <ul className={cn('files__list')}>
-                <li className={cn('files__list-item')}>
-                    <IconFilePdf />
-                    <a href="https://filebin.net/a0900c3i0uy8jpkg/168725.png" target="_blank" rel="noreferrer">Резюме</a>
-                </li>
-                <li className={cn('files__list-item')}>
-                    <IconFileDocument />
-                    <a href="https://filebin.net/a0900c3i0uy8jpkg/168725.png" target="_blank" rel="noreferrer">Полный список компетенций</a>
-                </li>
-                <li className={cn('files__list-item')}>
-                    <IconFileDocument />
-                    <a href="https://filebin.net/a0900c3i0uy8jpkg/168725.png" target="_blank" rel="noreferrer">Отзыв от работодателя</a>
-                </li>
-                <li className={cn('files__list-item')}>
-                    <IconFileImage />
-                    <a href="https://filebin.net/a0900c3i0uy8jpkg/168725.png" target="_blank" rel="noreferrer">Сертификат о повышении квалификации</a>
-                </li>
-            </ul>
+            {elList}
         </div>
     );
 };
