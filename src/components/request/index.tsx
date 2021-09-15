b0c8a3357184f602da54e446551935d623b91c25
Brezhnev Nikita <nvbrezhnev@sberbank.ru>
2021-09-15 10:58:13 +0300

    SC-16 add specialist button

--- src/components/request/index.tsx
+++ src/components/request/index.tsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { IStyle, useClassnames } from 'hook/use-classnames';
+import useModalClose from 'component/modal/use-modal-close';
 
 import Loader from 'component/loader';
-import { H3 } from 'component/header';
+import { H2, H3 } from 'component/header';
+import IconPlus from 'component/icons/plus';
+import IconApply from 'component/icons/apply';
+import Modal from 'component/modal';
+import IconClose from 'component/icons/close';
 
 import { mainRequest } from 'adapter/api/main';
 import { RequestRequirementRead } from 'adapter/types/main/request-requirement/get/code-200';
 
 import style from './index.module.pcss';
-import IconPlus from 'component/icons/plus';
-import IconApply from 'component/icons/apply';
 
 export interface IProps {
     className?: IStyle | string,
-    specialistId?: number
+    specialistId?: number,
+    showModal: boolean,
+    onClickClose?(): void
 }
 
 const Request = (props: IProps) => {
@@ -27,6 +32,18 @@ const Request = (props: IProps) => {
     const [addSpecialist, { isLoading: isLoadingAdd, isSuccess }] = mainRequest.usePostRequestRequirementLinkCvMutation();
 
     const [currentReqId, setCurrentReqId] = useState<number>();
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+    useEffect(() => {
+        setShowModal(props.showModal);
+    }, [props.showModal]);
+
+    useModalClose(showModal, setShowModal);
+
+    const onClickClose = () => {
+        setShowModal(false);
+        props.onClickClose?.();
+    };
 
     const onClickAddSpecialist = (requirementId?: number) => () => {
         if(requirementId) {
@@ -67,7 +84,11 @@ const Request = (props: IProps) => {
         }
 
         return (
-            <div className={cn('request__item-add-icon-wrapper')}>
+            <div
+                className={cn('request__item-add-icon-wrapper', {
+                    'request__item-add-icon-wrapper_success': isSuccess && currentReqId === reqId
+                })}
+            >
                 {content}
             </div>
         );
@@ -92,6 +113,19 @@ const Request = (props: IProps) => {
         );
     };
 
+    const elModalHeader = () => {
+        return (
+            <Fragment>
+                <H2 className={cn('request__modal-header-text')}>
+                    {t('routes.specialists.main.projects.add')}
+                </H2>
+                <div className={cn('request__modal-header-close')} onClick={onClickClose}>
+                    <IconClose svg={{ className: cn('request__modal-header-close-icon') }} />
+                </div>
+            </Fragment>
+        );
+    };
+
     const elContent = () => {
         if(isLoading) {
             return <Loader />;
@@ -109,11 +143,17 @@ const Request = (props: IProps) => {
         }
     };
 
-    return (
-        <div className={cn('request')}>
-            {elContent()}
-        </div>
-    );
+    if(showModal) {
+        return (
+            <Modal header={elModalHeader()}>
+                <div className={cn('request')}>
+                    {elContent()}
+                </div>
+            </Modal>
+        );
+    }
+
+    return null;
 };
 
 export default Request;
